2aa8f7ab8606a6be52c859e696923f1a0a0c0b60
ik012982i9 <ik012982i9@206-19-HZ336121.H012982.NET>
2018-02-06 11:18:55 +0100

    push 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-package com.paquete.operacion;
-
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-	}
 		
 
 	}
 
-
+}
---
+++ src/com/paquete/operacion/OperacionesAritmeticas.java
@@ -0,0 +1,55 @@
+package com.paquete.operacion;
+
+import java.util.Scanner;
+
+public class OperacionesAritmeticas {
+	
+	public float division(){
+		
+
+		int indice = 0;
+		
+		while(indice ==0){
+			
+		try {
+
+			Scanner scan = new Scanner(System.in);
+			Scanner scan1 = new Scanner(System.in);
+			String numerador_str;
+			String denominador_str;
+			float division1 = .0f;
+			int numerador, denominador;
+
+			System.out.println("Introduce el numerador");
+			numerador_str = scan.nextLine();
+			numerador = Integer.parseInt(numerador_str);
+
+			System.out.println("Introduce el denominador");
+			denominador_str = scan1.nextLine();
+			denominador = Integer.parseInt(denominador_str);
+
+			division1 = numerador / denominador;
+			
+			System.out.println(division1);
+			
+			indice =1;
+			
+		} catch (ArithmeticException ex) {
+			// Excepcion aritmetica en el caso de vividir entre 0 el valor es incorrecto
+			System.out.print("Introduce un valor");
+			
+		} catch (NullPointerException ex) {
+			// Excepcion si no se inserta ningun valor
+			System.out.print("Introduce un valor");
+		} catch (NumberFormatException ex) {
+			System.out.print("Introduce un numero");
+
+		}
+	}
+		return division();
+	}
+
+	
+	
+
+}
